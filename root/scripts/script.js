a38f5b7ccbfa2d1437d53b4bdfffc9ff1c150983
James.Holland <james.holland@oit.edu>
2026-06-21 23:27:47 -0700

    All quotes are now removed from array of stores.

--- root/scripts/script.js
+++ root/scripts/script.js
@@ -24,7 +24,7 @@ async function ParseCSV(path, arr) {
 	// iterate through list of lines and tokenize each word
 	for (let lineIndex = 1; lineIndex < lines.length; ++lineIndex) {
 		let cleanLine	  = lines[lineIndex].replaceAll('"', ''); // removing quotes
-		const tokens      = lines[lineIndex].split(",");		  // tokenizing using commmas
+		const tokens      = cleanLine.split(",");		  		  // tokenizing using commmas
 		
 		let name 	      = tokens[0];
 		let genre 	      = tokens[1];
